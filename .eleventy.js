cdd765768b7fc62f122f33b176088cb9cea74e6f
NoahS <86094302+boyder@users.noreply.github.com>
2024-04-14 20:06:26 -0400

    added content

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,22 @@
 // The export statement makes these settings available to other files in 11ty
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
+
+  eleventyConfig.addShortcode("postBox", function (post, date, time) {
+    return `
+  <div class="postHolder">
+    <div class="postBox">
+      <h2 class="postText">${post}</h2>
+      <button>
+        <h4>Click to view post</h4>
+      </button>
+      <div class="DateTime">
+        <h3>${time}</h3>
+        <h3>${date}</h3>
+      </div>
+    </div>
+  </div>
+    `;
+  })
 };
